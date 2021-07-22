310cfebfc12342beaa71d59917d547c97aa302f6
ashen007 <asheniranga@gmail.com>
2021-07-22 12:59:55 +0530

    get player classes

---
+++ player/getplayer.py
@@ -0,0 +1,73 @@
+class Test:
+    """
+    get test records for specific player
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get(self):
+        print('get player records.')
+
+    def save(self):
+        print('save player records.')
+
+
+class OneDay:
+    """
+    get test records for specific player
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get(self):
+        print('get player records.')
+
+    def save(self):
+        print('save player records.')
+
+
+class T20:
+    """
+    get test records for specific player
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get(self):
+        print('get player records.')
+
+    def save(self):
+        print('save player records.')
+
+
+class ASeries:
+    """
+    get test records for specific player
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get(self):
+        print('get player records.')
+
+    def save(self):
+        print('save player records.')
+
+
+class FirstClass:
+    """
+    get test records for specific player
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get(self):
+        print('get player records.')
+
+    def save(self):
+        print('save player records.')
