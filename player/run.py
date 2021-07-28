4be73497ae82cd2a652e8ae7037dda9f79968b2f
ashen007 <asheniranga@gmail.com>
2021-07-28 10:17:36 +0530

    add all countries players

---
+++ player/run.py
@@ -0,0 +1,9 @@
+from player.getplayer import players_details
+
+players_details('New Zealand')
+players_details('South Africa')
+players_details('India')
+players_details('Pakistan')
+players_details('Bangladesh')
+players_details('West Indies')
+players_details('Zimbabwe')
